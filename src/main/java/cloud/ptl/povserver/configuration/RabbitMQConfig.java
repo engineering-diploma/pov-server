2ba139faa1dab1f85fb7ac3f17c16706757b79fa
Piotr Tylczynski <piotr@ptl.cloud>
2021-08-14 18:58:32 +0200

    configuration externalize

--- src/main/java/cloud/ptl/povserver/configuration/RabbitMQConfig.java
+++ src/main/java/cloud/ptl/povserver/configuration/RabbitMQConfig.java
@@ -6,6 +6,7 @@ import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,14 +15,26 @@ import java.security.NoSuchAlgorithmException;
 
 @Configuration
 public class RabbitMQConfig {
+    @Value("${ptl.rabbit.host}")
+    private String host;
+
+    @Value("${ptl.rabbit.port}")
+    private int port;
+
+    @Value("${ptl.rabbit.username}")
+    private String username;
+
+    @Value("${ptl.rabbit.password}")
+    private String password;
+
     @Bean
     public ConnectionFactory connectionFactory() throws NoSuchAlgorithmException, KeyManagementException {
         com.rabbitmq.client.ConnectionFactory connection = new com.rabbitmq.client.ConnectionFactory();
-        connection.setHost("ptl.cloud");
-        connection.setPort(5671);
+        connection.setHost(this.host);
+        connection.setPort(this.port);
         connection.useSslProtocol();
-        connection.setUsername("pov");
-        connection.setPassword("pov11082021");
+        connection.setUsername(this.username);
+        connection.setPassword(this.password);
         return new CachingConnectionFactory(connection);
     }
 
