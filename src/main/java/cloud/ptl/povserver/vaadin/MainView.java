0d05d20268c2b9d0345064ab7730dd668b4a42a8
Piotr Tylczynski <piotr@ptl.cloud>
2021-08-17 22:04:24 +0200

    change: main view to utilize components not another views

--- src/main/java/cloud/ptl/povserver/vaadin/MainView.java
+++ src/main/java/cloud/ptl/povserver/vaadin/MainView.java
@@ -1,6 +1,7 @@
 package cloud.ptl.povserver.vaadin;
 
 
+import cloud.ptl.povserver.vaadin.utils.TabNameToContentMapper;
 import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.tabs.Tab;
@@ -11,8 +12,20 @@ import com.vaadin.flow.router.Route;
 public class MainView extends AppLayout {
     public MainView() {
         Image img = new Image("https://i.imgur.com/GPpnszs.png", "Vaadin Logo");
-        Tabs tabs = new Tabs(new Tab("Home"), new Tab("About"));
+        Tabs tabs = new Tabs(
+                new Tab("Home"),
+                new Tab("Search"),
+                new Tab("Queue")
+        );
         img.setHeight("44px");
+        setContent(TabNameToContentMapper.toContent("Home"));
+        tabs.addSelectedChangeListener(
+                l -> setContent(
+                        TabNameToContentMapper.toContent(
+                                tabs.getSelectedTab().getLabel()
+                        )
+                )
+        );
         addToNavbar(img, tabs);
     }
 }
