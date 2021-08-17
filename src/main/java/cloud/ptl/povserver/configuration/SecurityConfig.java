8b270cad5924d5102e36dc5ff7522ec39911c407
Piotr Tylczynski <piotr@ptl.cloud>
2021-08-17 18:19:50 +0200

    fix: move all security configuration into one class

--- src/main/java/cloud/ptl/povserver/configuration/SecurityConfig.java
+++ src/main/java/cloud/ptl/povserver/configuration/SecurityConfig.java
@@ -1,14 +1,19 @@
 package cloud.ptl.povserver.configuration;
 
+import com.vaadin.flow.server.ServletHelper;
+import com.vaadin.flow.shared.ApplicationConstants;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.stream.Stream;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -38,34 +43,48 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
-    @Configuration
-    @Order(1)
-    public static class ApiSecurity extends WebSecurityConfigurerAdapter {
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-            http
-                    .antMatcher("/api/**").authorizeRequests()
-                    .antMatchers("/api/**").authenticated()
-                    .and()
-                    .antMatcher("/api/**").httpBasic()
-                    .and()
-                    .csrf().disable();
-        }
+    static boolean isFrameworkInternalRequest(HttpServletRequest request) {
+        final String parameterValue = request.getParameter(ApplicationConstants.REQUEST_TYPE_PARAMETER);
+        return parameterValue != null
+                && Stream.of(ServletHelper.RequestType.values()).anyMatch(r -> r.getIdentifier().equals(parameterValue));
     }
 
-    @Configuration
-    @Order(2)
-    public static class PortalSecurity extends WebSecurityConfigurerAdapter {
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-            http
-                    .antMatcher("/portal/**").authorizeRequests()
-                    .antMatchers("/portal/**").authenticated()
-                    .antMatchers("/portal/**").hasRole("USER")
-                    .and()
-                    .formLogin()
-                    .and()
-                    .logout().permitAll();
-        }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/portal/**").hasRole("USER")
+                .regexMatchers("/frontend/.*", "/VAADIN/.*").permitAll()
+                .requestMatchers(SecurityConfig::isFrameworkInternalRequest).permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .and()
+                .httpBasic()
+                .and()
+                // vaadin already has csrf
+                .csrf().disable()
+                .logout().permitAll();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers(
+                // Vaadin Flow static resources //
+                "/VAADIN/**",
+                // the standard favicon URI
+                "/favicon.ico",
+                // the robots exclusion standard
+                "/robots.txt",
+                // web application manifest //
+                "/manifest.webmanifest",
+                "/sw.js",
+                "/offline-page.html",
+                // (development mode) static resources //
+                "/frontend/**",
+                // (development mode) webjars //
+                "/webjars/**",
+                // (production mode) static resources //
+                "/frontend-es5/**", "/frontend-es6/**");
     }
 }
