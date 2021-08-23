861d167c9a086c9e16b86425da42f7632efb6959
Piotr Tylczynski <piotr@ptl.cloud>
2021-08-24 01:29:52 +0200

    add: stream api

--- src/main/java/cloud/ptl/povserver/service/stream/StreamService.java
+++ src/main/java/cloud/ptl/povserver/service/stream/StreamService.java
@@ -1,6 +1,9 @@
 package cloud.ptl.povserver.service.stream;
 
+import cloud.ptl.povserver.data.model.ResourceDAO;
 import cloud.ptl.povserver.exception.NotFoundException;
+import cloud.ptl.povserver.ffmpeg.FfmpegService;
+import cloud.ptl.povserver.ffmpeg.ResizeRequest;
 import cloud.ptl.povserver.service.resource.ResourceService;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.core.io.FileUrlResource;
@@ -21,13 +24,31 @@ import static java.lang.Math.min;
 public class StreamService {
     private static final long CHUNK_SIZE = 1000000L;
     private final ResourceService resourceService;
+    private final FfmpegService ffmpegService;
 
-    public StreamService(ResourceService resourceService) {
+    public StreamService(ResourceService resourceService, FfmpegService ffmpegService) {
         this.resourceService = resourceService;
+        this.ffmpegService = ffmpegService;
     }
 
     public ResponseEntity<ResourceRegion> getVideoRegion(String rangeHeader, Long videoId) throws IOException, MalformedURLException, NotFoundException {
         String resourcePath = this.getVideoPath(videoId);
+        return this.extractRegion(rangeHeader, resourcePath);
+    }
+
+    public ResponseEntity<ResourceRegion> getVideoRegionResized(String rangeHeader, Long videoId, int width, int height) throws NotFoundException, IOException, InterruptedException {
+        ResourceDAO resourceDAO = this.resourceService.findById(videoId);
+        ResizeRequest resizeRequest = new ResizeRequest();
+        resizeRequest.setWidth(width);
+        resizeRequest.setHeight(height);
+        resizeRequest.setResourceDAO(resourceDAO);
+        ResourceDAO resizedResource = this.ffmpegService.resize(resizeRequest);
+
+        String resourcePath = resizedResource.getMovie().getAbsolutePath();
+        return this.extractRegion(rangeHeader, resourcePath);
+    }
+
+    private ResponseEntity<ResourceRegion> extractRegion(String rangeHeader, String resourcePath) throws IOException {
         FileUrlResource videoResource = new FileUrlResource(resourcePath);
         ResourceRegion resourceRegion = getResourceRegion(videoResource, rangeHeader);
 
