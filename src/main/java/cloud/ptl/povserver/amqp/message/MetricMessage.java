4f094f55c41ba2059a07ec00f2ee4fc45783f653
Piotr Tylczynski <piotr@ptl.cloud>
2021-08-26 15:13:51 +0200

    add: message documentation

--- src/main/java/cloud/ptl/povserver/amqp/message/MetricMessage.java
+++ src/main/java/cloud/ptl/povserver/amqp/message/MetricMessage.java
@@ -2,6 +2,9 @@ package cloud.ptl.povserver.amqp.message;
 
 import lombok.Data;
 
+/**
+ * Control message received from display. Contains basic informations about display state
+ */
 @Data
 public class MetricMessage {
     private String key;
