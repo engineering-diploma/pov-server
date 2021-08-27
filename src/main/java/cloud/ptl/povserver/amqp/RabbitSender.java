0668547aa6223001e90bd5cbcb246f851f5c0ab9
Piotr Tylczynski <piotr@ptl.cloud>
2021-08-27 19:52:22 +0200

    add: new logger for RabbitSender

--- src/main/java/cloud/ptl/povserver/amqp/RabbitSender.java
+++ src/main/java/cloud/ptl/povserver/amqp/RabbitSender.java
@@ -1,6 +1,8 @@
 package cloud.ptl.povserver.amqp;
 
 import cloud.ptl.povserver.amqp.message.VideoPingMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -11,6 +13,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class RabbitSender {
     private final RabbitTemplate rabbitTemplate;
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
     public RabbitSender(RabbitTemplate rabbitTemplate) {
@@ -23,6 +26,7 @@ public class RabbitSender {
      * @param videoPingMessage message to send to display, proper converter should be used
      */
     public void pingAboutNewVideo(VideoPingMessage videoPingMessage) {
+        this.logger.info("Sending video ping to display: " + videoPingMessage);
         this.rabbitTemplate.convertAndSend(
                 "server-to-pov-control-exchange",
                 "server-to-pov-control-binding",
