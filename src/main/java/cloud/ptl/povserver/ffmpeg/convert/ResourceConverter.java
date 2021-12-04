11cd01d41341a439fb5a199c6a98617e93583e02
ptylczynski <piotr@ptl.cloud>
2021-12-05 00:32:36 +0100

    change: made ResourceConverter abstract, add some common method across it implemeentations to base class

--- src/main/java/cloud/ptl/povserver/ffmpeg/convert/ResourceConverter.java
+++ src/main/java/cloud/ptl/povserver/ffmpeg/convert/ResourceConverter.java
@@ -1,11 +1,43 @@
 package cloud.ptl.povserver.ffmpeg.convert;
 
 import cloud.ptl.povserver.data.model.ResourceDAO;
+import lombok.extern.slf4j.Slf4j;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
-public interface ResourceConverter {
-    boolean supports(ConvertRequest.Format format);
+@Slf4j
+public abstract class ResourceConverter {
+    public abstract boolean supports(ConvertRequest.Format format);
 
-    ResourceDAO convert(ConvertRequest convertRequest) throws IOException, InterruptedException;
+    public abstract ResourceDAO convert(ConvertRequest convertRequest) throws IOException, InterruptedException;
+
+    public void run(String command) throws IOException, InterruptedException {
+        ProcessBuilder processBuilder = new ProcessBuilder(command.split(" "));
+        processBuilder.redirectErrorStream(true);
+        Process process = processBuilder.start();
+        // connect to process streams
+        BufferedReader outputBufferedReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        BufferedReader errorBufferedReader = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+
+        String line;
+
+        while ((line = outputBufferedReader.readLine()) != null) {
+            log.info(line);
+        }
+        while ((line = errorBufferedReader.readLine()) != null) {
+            log.info(line);
+        }
+
+        process.waitFor();
+        if (process.exitValue() == 0) {
+            // processed successfully
+            // rewrite data to resource
+            return;
+        } else {
+            // some error occurred
+            throw new RuntimeException("Cannot convert file to given resolution, ffmpeg exit code in none-zero");
+        }
+    }
 }
