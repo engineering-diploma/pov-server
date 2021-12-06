10494ca696e920dc5bc55464155d9e40774520a6
ptylczynski <piotr@ptl.cloud>
2021-12-06 03:33:45 +0100

    add: lookup via similar title to Resource repository

--- src/main/java/cloud/ptl/povserver/data/repositories/ResourceRepository.java
+++ src/main/java/cloud/ptl/povserver/data/repositories/ResourceRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.repository.CrudRepository;
 
 import java.io.File;
 import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
 public interface ResourceRepository extends CrudRepository<ResourceDAO, Long> {
@@ -19,6 +20,10 @@ public interface ResourceRepository extends CrudRepository<ResourceDAO, Long> {
     Optional<ResourceDAO> findByMovie(File file);
 
     Optional<ResourceDAO> findByTitleAndFormat(String title, ConvertRequest.Format format);
+
+    List<ResourceDAO> findAllByTitleContaining(String title);
+
+    List<ResourceDAO> findAllByTitleContainingAndFormat(String title, ConvertRequest.Format format);
 
     Collection<ResourceDAO> findAllByFormatEquals(ConvertRequest.Format format);
 
