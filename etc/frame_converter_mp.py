aeefc848485aa5ef5f72eca10992d65dfef2206b
opurie <52296677+opurie@users.noreply.github.com>
2021-12-11 13:56:37 +0100

    Update frame_converter_mp.py

--- etc/frame_converter_mp.py
+++ etc/frame_converter_mp.py
@@ -1,6 +1,8 @@
 import sys, cv2
 from multiprocessing import Pool, cpu_count, Lock
+from multiprocessing.pool import ThreadPool
 from PIL import Image
+from queue import Queue
 
 filename = sys.argv[1]
 outputname = sys.argv[2]
@@ -9,45 +11,60 @@ height = int(sys.argv[4])
 keying_freq = int(sys.argv[5])
 mp_count = cpu_count() # TODO insert cpu count as param(maybe)
 
-lock = Lock()
-video = cv2.VideoCapture(filename)
-fps = video.get(cv2.CAP_PROP_FPS)
-frame_count = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
-duration = (frame_count / fps) * 1000 / keying_freq  # video duration in milliseconds
 
-
-def algorithm(idx):
-    curr_step = int(duration / mp_count) * idx  # starting index
-    end = int(duration / mp_count) * (idx + 1)  # ending index
-    with open(f"{idx}_{outputname}", "w") as file:
+buffer = []
+def set_buffer():
+    video = cv2.VideoCapture(filename)
+    success, image = video.read()
+    step = 0
+    while success:
+        video.set(cv2.CAP_PROP_POS_MSEC, step * keying_freq)
         success, image = video.read()
-        while success:
-            try:
-                with lock:  # lock a video to extract frame
-                    video.set(cv2.CAP_PROP_POS_MSEC, curr_step * keying_freq)
-                    success, image = video.read()
-                    img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-            except:
-                success = False
-            if not success:
-                break
-            frame = Image.fromarray(img)
-            frame = frame.resize((width, height))
-            for i in range(frame.width):
-                for j in range(frame.height):
-                    pos = i, j
-                    px = frame.getpixel(pos)
-                    r = int(px[0])
-                    g = int(px[1])
-                    b = int(px[2])
-                    file.write(f"{r: <3} {g: <3} {b: <3} \t")
+        buffer.append([step, image])
+        step += 1
+
+    
+def save_to_file(buffer):
+    with open(outputname, "w") as file:
+        for idx, frame in buffer:
+            for row in frame:
+                for c in row:
+                    file.write(f"{c} ")
                 file.write("\n")
-            file.write('End of frame no. ' + str(curr_step) + '\n')
-            curr_step += 1
-            if curr_step == end and not idx == mp_count - 1:
-                break
+            file.write(f'End of frame no. {idx}\n')
+
+def parsepixel(pixel):
+    pass 
+
+
+def algorithm2(buf):
+    frame_idx, image = buf
+    if image is None:
+        return
+    img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    frame = Image.fromarray(img)
+    frame = frame.resize((width, height))
+    f = []
+    for i in range(frame.width):
+        row = []
+        for j in range(frame.height):
+            pos = i, j
+            px = frame.getpixel(pos)
+            r = int(px[0])
+            g = int(px[1])
+            b = int(px[2])
+            row.append(r)
+            row.append(g)
+            row.append(b)
+        f.append(row)
+    return frame_idx, f
 
 
 if __name__ == "__main__":
-    pool = Pool(mp_count)
-    pool.map(algorithm, range(mp_count))
+    set_buffer()
+    pool = ThreadPool(mp_count)
+    results = pool.map(algorithm2, buffer)
+    results.remove(None)
+    results.sort(key=lambda x:x[0])
+    save_to_file(results)
+    
